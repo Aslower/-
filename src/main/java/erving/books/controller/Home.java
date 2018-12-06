157bcbb26288ec77ab058da7cb4a26fb5b87450f
aslower <yeyuwei@m.scnu.edu.cn>
2018-12-06 19:43:33 +0800

    init

---
+++ src/main/java/erving/books/controller/Home.java
@@ -0,0 +1,13 @@
+package erving.books.controller;
+ 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+public class Home {
+ 
+    @RequestMapping("/")
+    public String home(){
+        return "你好，欢迎使用Visual Studio Code!ddssjaflkdjflk";
+    }
+}
